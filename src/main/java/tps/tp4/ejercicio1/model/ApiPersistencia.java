fa7b9e2ae9cff3228ab0605a44b7336bf7698c41
tonhy <antonhycorribolo@gmail.com>
2025-05-09 20:53:45 -0300

    cambio nombre en ApiPersistencia

--- src/main/java/tps/tp4/ejercicio1/model/ApiPersistencia.java
+++ src/main/java/tps/tp4/ejercicio1/model/ApiPersistencia.java
@@ -1,15 +1,15 @@
 package tps.tp4.ejercicio1.model;
 
 public class ApiPersistencia implements Persistencia {
-    private RegistroParticipante conexion;
+    private RegistroParticipante registrar;
 
-    public ApiPersistencia(RegistroParticipante conexion) {
-        this.conexion = conexion;
+    public ApiPersistencia(RegistroParticipante registrar) {
+        this.registrar = registrar;
     }
 
     @Override
     public void agregarParticipante(String nombre, String telefono, String region) {
         Participante participante = new Participante(nombre, telefono, region);
-        this.conexion.agregarParticipante(participante.nombre(), participante.telefono(), participante.region());
+        this.registrar.agregarParticipante(participante.nombre(), participante.telefono(), participante.region());
     }
 }
